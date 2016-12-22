c1f513515dea68e3a35cbecb82ebcaf77bea238d
Paul PANGANIBAN <ps.panganiban@gmail.com>
2016-12-22 17:04:08 +0100

    Rename folder projet to src and add the questuns answers

---
+++ src/PositionProtocol.java
@@ -0,0 +1,26 @@
+package projet;
+
+import peersim.edsim.EDProtocol;
+
+public interface PositionProtocol extends EDProtocol {
+	
+	/*Renvoie la coordonnée x, 0 <= x <=MaxX*/
+	public double getY();
+	
+	/*Renvoie la coordonnée y, 0 <= y <=MaxY */
+	public double getX();
+	
+	/*Renvoie la vitesse maximale qu'un noeud peut avoir*/
+	public int getMaxSpeed();
+	
+	/*Renvoie l'abscisse maximale (largeur du terrain)*/
+	public double getMaxX();
+	
+	
+	/*Renvoie l'ordonnée maximale (largeur du terrain)*/
+	public double getMaxY();
+	
+	
+	/*Renvoie le temps d'immobilité*/
+	public int getTimePause();
+}
